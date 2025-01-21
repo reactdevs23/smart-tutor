35ab4d6b52fead1e76e3b833543a4bb3685d533e
dalimkumar604 <dalimkumar212582@gmail.com>
2025-01-21 16:48:35 +0600

    verification

--- lib/email.ts
+++ lib/email.ts
@@ -1,25 +1,31 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   service: "Gmail",
   auth: {
-    user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD
-  }
+    user: "talrocktest@gmail.com",
+    pass: "rdxfqgahngmqpcep",
+  },
 });
 const sendMail = (to: string, subject: string, html: string) => {
   try {
     const mailOptions = {
-      from: "mdmehedihasan2360@gmail.com",
+      from: "talrocktest@gmail.com",
       to: to,
       subject: subject,
       html: html,
     };
 
-    transporter.sendMail(mailOptions);
+    transporter
+      .sendMail(mailOptions)
+      .catch((error) => {
+        console.log("Error send email: ", error);
+      })
+      .then((value) => {
+        console.log("Email sended");
+      });
   } catch (error) {
-    console.log("Error", error)
+    console.log("Error", error);
   }
-}
-
+};
 
 export default sendMail;
